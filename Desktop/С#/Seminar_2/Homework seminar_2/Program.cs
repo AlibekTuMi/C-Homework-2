0e2c56f7ba0250d6fc1f1781bc670f24b239a74a
Alibek <alibek.misalimov@yandex.ru>
2023-07-25 20:07:42 +0700

    Выполнили 2 задание

---
+++ Desktop/С#/Seminar_2/Homework seminar_2/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// 1 решение
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int num1 = num / 10;
+// int num2 = num1 % 10;
+
+//  Console.WriteLine(num2);
+
+// 2 решение
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int num1 = num / 100 * 100;
+// int num2 = (num - num1) - num % 10;
+// int num3 = num2 / 10;
+
+// Console.WriteLine(num3);
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int num = new Random().Next(10, 40000);
+Console.WriteLine(num);
+
+
+if (num<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if (num > 99 && num < 1000)
+{
+    num = num% 10;
+    Console.WriteLine(num);
+}
+else if (num > 999 && num <10000)
+{
+    num = num /10 %10;
+    Console.WriteLine(num);
+}
+else if (num > 9999 && num <40000)
+{
+    num = num /100 %10;
+    Console.WriteLine(num);
+}
