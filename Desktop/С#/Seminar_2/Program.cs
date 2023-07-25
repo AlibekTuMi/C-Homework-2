07cf9c8f8f05be0cf5e535f44f0b57cd481d0256
Alibek <alibek.misalimov@yandex.ru>
2023-07-25 17:44:43 +0700

    Первое задание

---
+++ Desktop/С#/Seminar_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая выводит случайное число из
+// отрезка [10, 99] и показывает наибольшую цифру числа.
+
+// Например:
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int num = new Random().Next(10, 99);
+Console.WriteLine(num);
+
+int num1 = num / 10;
+int num2 = num % 10;
+
+if(num1>num2)
+{
+    Console.WriteLine("Число" + num1+ "больше чем"); // канкатинация
+}
+else if(num1==num2)
+{
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    Console.WriteLine($"Число {num2} больше чем");  // интерполяция
+}
