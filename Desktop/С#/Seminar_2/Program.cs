ca45bcfdf647a05ce65e1f822195b3c63555fc87
Alibek <alibek.misalimov@yandex.ru>
2023-07-25 18:15:39 +0700

    Не решенное задание, найти ошибку

--- Desktop/С#/Seminar_2/Program.cs
+++ Desktop/С#/Seminar_2/Program.cs
@@ -32,11 +32,35 @@
 // 782->72
 // 918->98
 
-int num = new Random().Next(100, 1000);
-Console.WriteLine(num);
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
 
-int num1 = num / 100;
-int num2 = num % 10;
-int result = num1 * 10 + num2;
+// int num1 = num / 100;
+// int num2 = num % 10;
+// int result = num1 * 10 + num2;
 
-Console.WriteLine(result);
+// Console.WriteLine(result);
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число 
+// кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.Readline());
+
+Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.Readline());
+
+if(num % num1 == 0)
+{
+    Console.WriteLine("Числа кратные");
+}
+else
+{
+    Console.WriteLine("Числа не кратные, остаток равен" + num % num1);
+}
