32f0864e78f4ac1088184eb9a4306d15cd8e93df
Alibek <alibek.misalimov@yandex.ru>
2023-07-28 19:48:11 +0700

    Выполнил 3 ДЗ

--- Desktop/С#/Homework seminar_3/Program.cs
+++ Desktop/С#/Homework seminar_3/Program.cs
@@ -25,24 +25,38 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите число X1 : ");
-int X1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число X1 : ");
+// int X1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число Y1 : " );
-int Y1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число Y1 : " );
+// int Y1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число Z1 : ");
-int Z1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число Z1 : ");
+// int Z1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число X2 : " );
-int X2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число X2 : " );
+// int X2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число Y2 : ");
-int Y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число Y2 : ");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число Z2 : " );
-int Z2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число Z2 : " );
+// int Z2 = Convert.ToInt32(Console.ReadLine());
 
-double s =Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)+ Math.Pow((Z2 - Z1), 2));
+// double s =Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2)+ Math.Pow((Z2 - Z1), 2));
 
-System.Console.WriteLine(s);
+// System.Console.WriteLine(s);
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i<=num; i++)
+{
+    double s = Math.Pow(i,3);
+    Console.Write(s);
+}
