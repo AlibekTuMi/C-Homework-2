ba1f1143de7b7734c31c97f65bdb71fdddf22de5
Alibek <alibek.misalimov@yandex.ru>
2023-08-05 00:22:39 +0700

    Выполнил 2 задачу

--- Desktop/С#/Homework seminar_4/Program.cs
+++ Desktop/С#/Homework seminar_4/Program.cs
@@ -3,15 +3,40 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-Console.WriteLine("Введите возводимое число : ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите возводимое число : ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число, в которое хотите возвести : ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число, в которое хотите возвести : ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-double s =Math.Pow((num1), num2);
+// double s =Math.Pow((num1), num2);
 
-System.Console.WriteLine(s);
+// System.Console.WriteLine(s);
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNum()
+{
+    int sum = 0; 
+    while (num > 0) 
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(GetSumNum());
+
+
 
 
 
