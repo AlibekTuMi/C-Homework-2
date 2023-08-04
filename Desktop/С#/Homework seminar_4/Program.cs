859d16cc577510bdaeae9875aa37281b75dc8167
Alibek <alibek.misalimov@yandex.ru>
2023-08-05 00:54:59 +0700

    Выполнил 3 задание

--- Desktop/С#/Homework seminar_4/Program.cs
+++ Desktop/С#/Homework seminar_4/Program.cs
@@ -20,24 +20,39 @@
 // 82 -> 10
 // 9012 -> 12
 
-Console.WriteLine("Введите число : ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число : ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-int GetSumNum()
+// int GetSumNum()
+// {
+//     int sum = 0; 
+//     while (num > 0) 
+//     {
+//         sum += num % 10;
+//         num /= 10;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine(GetSumNum());
+
+
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int [] array = new int[8];
+GetArray();
+
+void GetArray()
 {
-    int sum = 0; 
-    while (num > 0) 
+    for (int i = 0; i < array.Length; i++)
     {
-        sum += num % 10;
-        num /= 10;
+        array[i] = new Random().Next(0,50);        
+        Console.Write(array[i]+" ");
     }
-    return sum;
 }
-
-Console.WriteLine(GetSumNum());
-
-
-
 
 
 
