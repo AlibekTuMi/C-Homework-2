5e0406fd714b629cf1d811a09c526f9342ae2b13
Alibek <alibek.misalimov@yandex.ru>
2023-08-04 22:10:05 +0700

    Выполнил 1 задание

---
+++ Desktop/С#/Homework seminar_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в 
+// натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите возводимое число : ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число, в которое хотите возвести : ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+double s =Math.Pow((num1), num2);
+
+System.Console.WriteLine(s);
+
+
+
+
+
