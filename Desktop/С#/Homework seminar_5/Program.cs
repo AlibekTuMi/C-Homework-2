f30cd50b1bde990cca807355ed9c5b98425e710d
Alibek <alibek.misalimov@yandex.ru>
2023-08-05 21:15:18 +0700

    Задача 3 не решенаgit add Program.cs !

--- Desktop/С#/Homework seminar_5/Program.cs
+++ Desktop/С#/Homework seminar_5/Program.cs
@@ -28,24 +28,48 @@
 // [-4, -6, 89, 6] -> 0
 
 
-int[] array = new int[4];
-int sum = 0;
+// int[] array = new int[4];
+// int sum = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(0,11);
+//     Console.Write(array[i] + " ");
+
+//     if (i % 2 == 0)
+//     {
+//         sum = sum + 0;
+//     }
+//     else
+//     {
+//         sum = sum + array[i];
+//     }
+// }
+// System.Console.WriteLine();
+// System.Console.Write("Сумма чисел на нечётных позициях " +sum);
+
+
+// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов 
+// массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+
+double[] array = new double[4.43, 5.78, 7.62, 5.45, 12.2, 24.8, 13.1];
+
+double Max = Max[i];
+double Min = Min[i];
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(0,11);
-    Console.Write(array[i] + " ");
+    if (i > i + 1)
+    {
+        i = Max;
 
-    if (i % 2 == 0)
+    }                                       //          КОД НЕ РАБОТАЕТ, НЕ ЗНАЮ ЧТО ДЕЛАЬ С DOUBLE!!!!!!!!!!!
+    else if (i < i + 1)
     {
-        sum = sum + 0;
-    }
-    else
-    {
-        sum = sum + array[i];
+        i = Min;
     }
 }
-System.Console.WriteLine();
-System.Console.Write("Сумма чисел на нечётных позициях " +sum);
-
-
+double result = Max - Min;
+System.Console.WriteLine(result);
