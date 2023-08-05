d212578cfd0c4dc51d660249a3592a5bc322ce5e
Alibek <alibek.misalimov@yandex.ru>
2023-08-05 19:38:34 +0700

    Выполнил 2 задание

--- Desktop/С#/Homework seminar_5/Program.cs
+++ Desktop/С#/Homework seminar_5/Program.cs
@@ -2,21 +2,50 @@
 // которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] array = new int[10];
-int count = 0;
+// int[] array = new int[10];
+// int count = 0;
+
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(100, 1000);
+//     Console.Write(array[i] + " ");
+//     if (array[i] / 2 * 2 < array[i])
+//     {
+//         count = count + 0;
+//     }
+//     else
+//     {
+//         count++;        
+//     }
+
+// }
+// System.Console.WriteLine(", Чётных чисел " +count);
+
+
+//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+//  стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] array = new int[4];
+int sum = 0;
 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next(100, 1000);
+    array[i] = new Random().Next(0,11);
     Console.Write(array[i] + " ");
-    if (array[i] / 2 * 2 < array[i])
+
+    if (i % 2 == 0)
     {
-        count = count + 0;
+        sum = sum + 0;
     }
     else
     {
-        count++;        
+        sum = sum + array[i];
     }
-
 }
-System.Console.WriteLine(",Чётных чисел " +count);
+System.Console.WriteLine();
+System.Console.Write("Сумма чисел на нечётных позициях " +sum);
+
+
