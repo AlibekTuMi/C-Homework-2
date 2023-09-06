fcc33de8dc0d7b9282a21967629ac53ab28d7ea7
Alibek <alibek.misalimov@yandex.ru>
2023-09-06 14:56:04 +0700

    Выполнил 1 задание

---
+++ Desktop/С#/Homework seminar_9/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5->"5,4,3,2,1"
+// N = 8->"8,7,6,5,4,3,2,1"
+
+
+System.Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+int startNum = 1;
+ 
+Console.WriteLine(PrintNumbers(num,startNum));
+
+string PrintNumbers(int end, int start)
+{
+    if (end == start)
+    {
+        return end.ToString();
+    }
+    return (end + " " + PrintNumbers(end - 1,start)); 
+}
